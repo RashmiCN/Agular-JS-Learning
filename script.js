232d9bb254a2c3bd6e6570baff435aee40a02f45
RashmiCN <rashmichudamani@gmail.com>
2018-01-14 23:50:58 +0530

    the JSFile V1

---
+++ script.js
@@ -0,0 +1,38 @@
+// Code goes here
+(function() {
+
+  var app = angular.module("githubViewer", []);
+
+  var MainController = function($scope, $http) {
+
+    var onUserComplete = function(response) {
+      $scope.user = response.data;
+      $http.get($scope.user.repos_url)
+        .then(onRepos, onError);
+    };
+
+    var onError = function(reason) {
+      $scope.error = "Could not fetch the user";
+    };
+
+
+    $scope.search = function(username) {
+      $http.get("https://api.github.com/users/" + username)
+        .then(onUserComplete, onError);
+    };
+
+    var onRepos = function(response) {
+      $scope.repos = response.data;
+    };
+
+
+    $scope.username = "Angular";
+    $scope.message = "Github Viewer";
+    $scope.repoSortOrder=["language","-stargazers_count"];
+
+
+  };
+
+  app.controller("MainController", ["$scope", "$http", MainController]);
+
+}());
